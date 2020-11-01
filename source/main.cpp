8d9d486a9db436d430a4962f70ef0dc4f7c9ac30
Luis Scheurenbrand <Luisscheurenbrand@gmail.com>
2020-11-01 01:54:56 +0100

    confirm network availabiltiy with nifm

--- source/main.cpp
+++ source/main.cpp
@@ -2,75 +2,70 @@
 #include <cstdio>
 #include <cstring>
 
-static Result _nsCmdInU64OutAsyncResult(Service* srv, AsyncResult *a, u64 inval, u32 cmd_id) {
-    memset(a, 0, sizeof(*a));
-    Handle event = INVALID_HANDLE;
-    Result rc = serviceDispatchIn(srv, cmd_id, inval,
-        .out_num_objects = 1,
-        .out_objects = &a->s,
-        .out_handle_attrs = { SfOutHandleAttr_HipcCopy },
-        .out_handles = &event,
-    );
-
-    if (R_SUCCEEDED(rc))
-        eventLoadRemote(&a->event, event, false);
-
-    return rc;
-}
-
-static Result nsRequestUpdateApplication2Shim(AsyncResult *a, u64 application_id) {
-    Service srv={0};
-    Result rc=0;
-    rc = nsGetApplicationManagerInterface(&srv);
-
-    if (R_SUCCEEDED(rc)) rc = _nsCmdInU64OutAsyncResult(&srv, a, application_id, 85);
-
-    serviceClose(&srv);
-    return rc;
-}
-
 int main()
 {
     consoleInit(nullptr);
 
+    /* Initiate Services. */
     nsInitialize();
+    nifmInitialize(NifmServiceType_System);
 
-    s32 offset=0, count=0;
-    NsApplicationRecord record;
-    while (R_SUCCEEDED(nsListApplicationRecord(&record, 1, offset++, &count)) && count != 0) {
-        /* Skip archived applications. */
-        if (record.type == 0xb)
-            continue;
+    /* Make network request. */
+    NifmRequest request;
+    if (R_SUCCEEDED(nifmCreateRequest(&request, true))) {
+        nifmRequestSubmitAndWait(&request);
+        
+        /* Confirm network availability. */
+        if (nifmIsAnyInternetRequestAccepted(nifmGetClientId())) {
 
-        printf("Checking %016lX...", record.application_id);
-        consoleUpdate(nullptr);
+            /* Iterate over installed applications. */
+            s32 offset=0, count=0;
+            NsApplicationRecord record;
+            while (R_SUCCEEDED(nsListApplicationRecord(&record, 1, offset++, &count)) && count != 0) {
+                /* Skip archived applications. */
+                if (record.type == 0xb)
+                    continue;
 
-        /* Request update. */
-        AsyncResult async;
-        if (R_SUCCEEDED(nsRequestUpdateApplication2Shim(&async, record.application_id))) {
-            
-            /* Wait for result. */
-            asyncResultWait(&async, UINT64_MAX);
-            if (R_SUCCEEDED(asyncResultGet(&async))) {
-                static NsApplicationControlData nacp;
-                NacpLanguageEntry *entry = &nacp.nacp.lang[0];
-                u64 size=0;
-                nsGetApplicationControlData(NsApplicationControlSource_Storage, record.application_id, &nacp, sizeof(nacp), &size);
-                nacpGetLanguageEntry(&nacp.nacp, &entry);
-                printf("\nUpdating: %s\n", entry->name);
-            } else {
-                printf(" No update found\n");
+                printf("Checking %016lX...", record.application_id);
+                consoleUpdate(nullptr);
+
+                /* Request update. */
+                AsyncResult async;
+                if (R_SUCCEEDED(nsRequestUpdateApplication2(&async, record.application_id))) {
+                    
+                    /* Wait for result. */
+                    asyncResultWait(&async, UINT64_MAX);
+                    if (R_SUCCEEDED(asyncResultGet(&async))) {
+                        static NsApplicationControlData nacp;
+                        NacpLanguageEntry *entry = &nacp.nacp.lang[0];
+                        u64 size=0;
+                        nsGetApplicationControlData(NsApplicationControlSource_Storage, record.application_id, &nacp, sizeof(nacp), &size);
+                        nacpGetLanguageEntry(&nacp.nacp, &entry);
+                        printf("\nUpdating: %s\n", entry->name);
+                    } else {
+                        printf(" No update found\n");
+                    }
+                    asyncResultClose(&async);
+                } else {
+                    printf(" Update in progress\n");
+                }
             }
-            asyncResultClose(&async);
         } else {
-            printf(" Update in progress\n");
+            printf("Network connectivity couldn't be established.\nAborting!\n");
         }
+        
+        /* Close network request. */
+        nifmRequestClose(&request);
+    } else {
+        printf("Failed to make a network request.\nAborting!\n");
     }
+
+    /* Exit services. */
+    nifmExit();
+    nsExit();
 
     printf("Done!\nPress + to exit.\n");
     consoleUpdate(nullptr);
-
-    nsExit();
 
     while (appletMainLoop()) {
         hidScanInput();
